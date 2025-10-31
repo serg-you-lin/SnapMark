f175f3f0b2bb2b5e06fb105d07f74fd12176af2c
Federico <smia46@virgilio.it>
2025-10-31 13:05:20 +0100

    🔥 Remove Examples folders and ignore them

--- examples/examples/run_bak_example.py
+++
@@ -1,9 +0,0 @@
-import snapmark as sm
-
-folder_path = r'Examples/Input'
-sequence = sm.Conc(sm.Conc.FIRST_FOLDER_CHAR, sm.Conc.FILE_NAME)
-print('sequenza', sequence)
-
-ToMark = sm.iteration_manager(folder_path, use_backup_system=True)
-ToMark.add_operation(sm.AddMark(sequence, min_char=10, max_char=20, align='c', scale_factor=100, down_to=2))
-ToMark.file_selection_logic()
--- examples/examples/test.py
+++
@@ -1,8 +0,0 @@
-# Nel file RunExample.py (o crea test.py)
-import snapmark as sm
-
-print("Versione SnapMark:", sm.__version__)
-print("SnapMark Author:", sm.__author__)
-
-# Test quick function
-# sm.quick_mark("Examples/Input", sequence_type='file_name', scale_factor=100)
